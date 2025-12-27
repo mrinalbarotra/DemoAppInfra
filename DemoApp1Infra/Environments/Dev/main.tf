687e0835879947075054c2d3ef4901b64342c3de
mrinalbarotra <mrinalbarotra@gmail.com>
2025-12-27 05:35:42 +0530

    update the infra code

--- DemoApp1Infra/Environments/Dev/main.tf
+++ DemoApp1Infra/Environments/Dev/main.tf
@@ -13,8 +13,15 @@ provider "azurerm" {
   features {}
 }
 
+# plugin "azurerm" {
+#   enabled = true
+#   version = "0.26.0"
+#   source  = "github.com/terraform-linters/tflint-ruleset-azurerm"
+# }
+
+
 module "rgs" {
-  source = "../Modules/azurerm_resource_group"
+  source = "../../Modules/azurerm_resource_group"
   rgs    = var.rgs
   tags   = local.common_tags
 
@@ -22,20 +29,20 @@ module "rgs" {
 
 module "vnets" {
   depends_on = [module.rgs]
-  source     = "../Modules/azurerm_virtual_network"
+  source     = "../../Modules/azurerm_virtual_network"
   vnets      = var.vnets
   tags       = local.common_tags
 }
 
 module "subnets" {
   depends_on = [module.rgs, module.vnets]
-  source     = "../Modules/azurerm_subnet"
+  source     = "../../Modules/azurerm_subnet"
   subnets    = var.subnets
 }
 
 module "nsgs" {
   depends_on = [module.rgs]
-  source     = "../Modules/azurerm_nsg"
+  source     = "../../Modules/azurerm_nsg"
   nsgs       = var.nsgs
   tags       = local.common_tags
 
@@ -43,8 +50,8 @@ module "nsgs" {
 
 
 module "subnet_nsg_assoc" {
-  depends_on = [module.rgs, module.subnets]
-  source     = "../Modules/azurerm_subnet_nsg_association"
+  depends_on = [module.nsgs, module.subnets]
+  source     = "../../Modules/azurerm_subnet_nsg_association"
 
   nsgtosubnets = {
     subnet1_assoc = {
@@ -61,7 +68,7 @@ module "subnet_nsg_assoc" {
 
 module "vms" {
   depends_on = [module.rgs, module.subnets, ]
-  source     = "../Modules/azurerm_virtual_machine"
+  source     = "../../Modules/azurerm_virtual_machine"
   vms        = local.vms
   tags       = local.common_tags
 
@@ -70,20 +77,20 @@ module "vms" {
 
 # module "acrs" {
 #   depends_on = [ module.rgs ]
-#   source = "../Modules/azurerm_acr"
+#   source = "../../Modules/azurerm_acr"
 #   acrs = var.acrs
 # }
 
 module "mssqlservers" {
   depends_on   = [module.rgs]
-  source       = "../Modules/azurerm_mssqlserver"
+  source       = "../../Modules/azurerm_mssqlserver"
   mssqlservers = var.mssqlservers
   tags         = local.common_tags
 }
 
 module "mssqldatabase" {
   depends_on     = [module.rgs, module.mssqlservers]
-  source         = "../Modules/azurerm_mssql_database"
+  source         = "../../Modules/azurerm_mssql_database"
   mssqldatabases = var.mssqldatabases
   server_id      = module.mssqlservers.server_ids["sql1"]
   tags           = local.common_tags
@@ -91,7 +98,7 @@ module "mssqldatabase" {
 
 module "pip" {
   depends_on = [module.rgs]
-  source     = "../Modules/azurerm_pip"
+  source     = "../../Modules/azurerm_pip"
   pips       = var.pips
   tags       = local.common_tags
 
@@ -99,13 +106,13 @@ module "pip" {
 
 # module "sas" {
 #   depends_on = [module.rgs]
-#   source     = "../Modules/azurerm_storage_account"
+#   source     = "../../Modules/azurer../m_storage_account"
 #   stgs       = var.stgs
 # }
 
 # module "kvs" {
 #   depends_on = [module.rgs]
-#   source     = "../Modules/azurerm_key_vault"
+#   source     = "../../Modules/azurerm_key_vault"
 #   kvs        = var.kvs
 #   tenant_id = data.azurerm_client_config.current.tenant_id
 # }
