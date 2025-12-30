8cedcd905d5b72e9648d9f791c02107cc246d274
mrinalbarotra <mrinalbarotra@gmail.com>
2025-12-31 04:55:37 +0530

    corrected terraform code

--- DemoApp1Infra/Modules/azurerm_mssqlserver/data.tf
+++ DemoApp1Infra/Modules/azurerm_mssqlserver/data.tf
@@ -1,9 +1,11 @@
 data "azuread_group" "sql_admin" {
+  depends_on = [ azurerm_mssql_server.mssqlserver ]
   for_each     = var.mssqlservers
   display_name = each.value.azuread_administrator.ad_group_name
 }
 
 data "azurerm_mssql_server" "sql1" {
+  depends_on = [ azurerm_mssql_server.mssqlserver ]
   name                = "dev-demoapp1-centralindia-mssqlserver1"
   resource_group_name = "dev-demoapp1-centralindia-rg1"
 
