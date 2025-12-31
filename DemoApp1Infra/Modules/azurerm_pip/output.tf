c03301b506c32826fdf128d66830422de0c10dbd
mrinalbarotra <mrinalbarotra@gmail.com>
2025-12-31 05:42:38 +0530

    corrected terraform code

--- DemoApp1Infra/Modules/azurerm_pip/output.tf
+++ DemoApp1Infra/Modules/azurerm_pip/output.tf
@@ -1,7 +1,14 @@
+# output "public_ip_ids" {
+#   description = "Map of Public IP addresses"
+#   value = {
+#     for k, v in azurerm_public_ip.pip :
+#     k => v.ip_address
+#   }
+# }
+
 output "public_ip_ids" {
-  description = "Map of Public IP addresses"
   value = {
     for k, v in azurerm_public_ip.pip :
-    k => v.ip_address
+    k => v.id
   }
 }
