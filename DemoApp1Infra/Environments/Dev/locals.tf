7594841efdc806d6c0cb2ad12d87bebed8e00a24
mrinalbarotra <mrinalbarotra@gmail.com>
2026-01-01 06:17:05 +0530

    corrected terraform code

--- DemoApp1Infra/Environments/Dev/locals.tf
+++ DemoApp1Infra/Environments/Dev/locals.tf
@@ -27,7 +27,7 @@ locals {
 
       source_image_reference = {
         publisher = "Canonical"
-        offer     = "0001-com-ubuntu-server-focal-jammy"
+        offer     = "0001-com-ubuntu-server-jammy"
         sku       = "22_04-lts"
         version   = "latest"
       }
