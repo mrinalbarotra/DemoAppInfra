da6e576f4fff069c3a75d1e4c2f1357df7bc8d1f
mrinalbarotra <mrinalbarotra@gmail.com>
2026-01-24 12:11:08 +0530

    Updated code with new infra

--- DemoApp1Infra/Modules/azurerm_mssqlserver/data.tf
+++ DemoApp1Infra/Modules/azurerm_mssqlserver/data.tf
@@ -4,9 +4,9 @@ data "azuread_group" "sql_admin" {
   display_name = each.value.azuread_administrator.ad_group_name
 }
 
-data "azurerm_mssql_server" "sql1" {
-  # depends_on = [ azurerm_mssql_server.mssqlserver ]
-  name                = "dev-demoapp1-centralindia-mssqlserver1"
-  resource_group_name = "dev-demoapp1-centralindia-rg1"
+# data "azurerm_mssql_server" "sql1" {
+#   # depends_on = [ azurerm_mssql_server.mssqlserver ]
+#   name                = "dev-demoapp1-centralindia-mssqlserver1"
+#   resource_group_name = "dev-demoapp1-centralindia-rg1"
 
-}
+# }
